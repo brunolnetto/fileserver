2e36e9157deef1f15fa0f2ee2be968044d81a46b
Bruno Peixoto <brunolnetto@gmail.com>
2024-07-19 17:46:56 -0300

    feat: file uploader, the beginning

--- web/urls.py
+++ web/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include 
-from .views import upload_view, upload_success
+from .views import upload_view, upload_success, upload_status
 
 urlpatterns = [
     path('upload/', upload_view, name='upload'),
     path('success/', upload_success, name='upload_success'),
+    path('status/', upload_status, name='upload_status'),
+
 ]
 
--- web/views.py
+++ web/views.py
@@ -1,15 +1,27 @@
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
+
 from .forms import UploadForm
+from .models import Upload
 
 def upload_view(request):
     if request.method == 'POST':
         form = UploadForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('upload_success')
+            return redirect('success')
     else:
         form = UploadForm()
     return render(request, 'web/upload.html', {'form': form})
 
 def upload_success(request):
     return render(request, 'web/success.html')
+
+def upload_status(request):
+    uploads_list = Upload.objects.all()
+    paginator = Paginator(uploads_list, 10)  # Show 10 uploads per page
+
+    page_number = request.GET.get('page')
+    uploads = paginator.get_page(page_number)
+
+    return render(request, 'web/status.html', {'uploads': uploads})
